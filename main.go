48c07365992abebc97054fd2067c6133eb04aba3
vidarlx <lax@fea.st>
2018-01-14 22:32:29 +0100

    Shell fields from response

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Properties struct {
@@ -18,22 +20,65 @@ type SensorReads struct {
 	SensorData []Properties `json:"sensordatavalues"`
 }
 
+type Record struct {
+	PM25        float64
+	PM10        float64
+	Temperature float64
+	Humidity    float64
+	Date        time.Time
+}
+
 func dataHandler(w http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(body))
+	//fmt.Println(string(body))
 	var t SensorReads
 	err = json.Unmarshal(body, &t)
 	if err != nil {
 		panic(err)
 	}
 
-	for k, v := range t.SensorData {
-		fmt.Println(k, v.ValueType, v.Value)
+	var r Record
+	r.Date = time.Now()
+	for _, v := range t.SensorData {
+		switch v.ValueType {
+		case "SDS_P1":
+			val, e := strconv.ParseFloat(v.Value, 64)
+			if e != nil {
+				fmt.Println(e)
+			}
+
+			r.PM10 = val
+			break
+
+		case "SDS_P2":
+			val, e := strconv.ParseFloat(v.Value, 32)
+			if e != nil {
+				fmt.Println(e)
+			}
+			r.PM25 = val
+			break
+
+		case "temperature":
+			val, e := strconv.ParseFloat(v.Value, 32)
+			if e != nil {
+				fmt.Println(e)
+			}
+			r.Temperature = val
+			break
+		case "humidity":
+			val, e := strconv.ParseFloat(v.Value, 32)
+			if e != nil {
+				fmt.Println(e)
+			}
+			r.Humidity = val
+			break
+		}
+		//fmt.Println(k, v.ValueType, v.Value)
 	}
-	// fmt.Println(t)
+	fmt.Println(r)
 }
 
 func main() {
