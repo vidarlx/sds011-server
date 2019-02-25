b81951eb7bddac474853a3dae95e3e41b6581735
Sean Doe <lax@fea.st>
2019-02-25 22:48:17 +0100

    Linter fixed

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/ghanto/sds011-server
 
 require (
 	github.com/apsdehal/go-logger v0.0.0-20180929234804-e1d9ff37593f
+	github.com/golang/lint v0.0.0-20181217174547-8f45f776aaf1 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.3.0
+	golang.org/x/lint v0.0.0-20181217174547-8f45f776aaf1 // indirect
+	golang.org/x/tools v0.0.0-20190221204921-83362c3779f5 // indirect
 )
--- service/mock_sensor_repository.go
+++ service/mock_sensor_repository.go
@@ -6,11 +6,13 @@ import (
 	sensor "github.com/ghanto/sds011-server/entity"
 )
 
+// MockSensorRepository mock repository structure
 type MockSensorRepository struct {
 	records []sensor.Record
 	err     error
 }
 
+// NewMockRepository creates a new instance of mock repository
 func NewMockRepository(r []sensor.Record, e error) *MockSensorRepository {
 	return &MockSensorRepository{
 		records: r,
@@ -18,10 +20,12 @@ func NewMockRepository(r []sensor.Record, e error) *MockSensorRepository {
 	}
 }
 
+// Add an item to the repository
 func (s *MockSensorRepository) Add(ctx context.Context, record sensor.Record) error {
 	return s.err
 }
 
+// Get get all items from repository
 func (s *MockSensorRepository) Get(ctx context.Context) ([]sensor.Record, error) {
 	if s.err != nil {
 		return nil, s.err
